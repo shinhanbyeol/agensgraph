17abb7b7ca21df416e49d5b9bf6696f6b3935e1c
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 00:19:41 +0300

    Hook for custom error cleanup

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -2624,6 +2624,7 @@ AbortTransaction(void)
 	 * while cleaning up!
 	 */
 	LWLockReleaseAll();
+	CustomErrorCleanup();
 
 	/* Clear wait information and command progress indicator */
 	pgstat_report_wait_end();
@@ -4953,6 +4954,7 @@ AbortSubTransaction(void)
 	 * Buffer locks, for example?  I don't think so but I'm not sure.
 	 */
 	LWLockReleaseAll();
+	CustomErrorCleanup();
 
 	pgstat_report_wait_end();
 	pgstat_progress_end_command();
